95c6d8cc36636fa74e931a59bc728ce097a078c3
Lucas Miranda <lucas-m@outlook.com>
2020-03-28 18:02:01 -0300

    added event listeners on previous and next images in the carousel

--- js/script.js
+++ js/script.js
@@ -11,29 +11,58 @@
     items[currentIndex].style.transform = 'scale(1)'
 
   const next = () => {
-    console.log(currentIndex)
+    // if we are in the end, do nothing
     if (currentIndex+1 >= items.length) {
       return
     }
+    // remove event listener set before and update styles
+    items[currentIndex+1].removeEventListener('click', next)
+    if (currentIndex-1 >=0) items[currentIndex-1].removeEventListener('click', previous)
     items[currentIndex].removeAttribute('style')
+    // update the current index
     currentIndex++
+    // add new event listeners based on current position and update styles
     carousel.style.transform = `translateX(-${initialOffset + 876*(currentIndex-1)}px)`
     items[currentIndex].style.transform = 'scale(1)'
+    if (currentIndex+1 < items.length) items[currentIndex+1].addEventListener('click', next)
+    items[currentIndex-1].addEventListener('click', previous)
   }
 
   const previous = () => {
-    console.log(currentIndex)
+    // if we are in the start, do nothing
     if (currentIndex <= 0) {
       return
     }
+    // remove event listener set before and update styles
+    if (currentIndex+1 < items.length) items[currentIndex+1].removeEventListener('click', next)
+    items[currentIndex-1].removeEventListener('click', previous)
     items[currentIndex].removeAttribute('style')
+    // update the current index
     currentIndex--
+    // add new event listeners based on current position and update styles
     const offset = currentIndex === 0 ? 0 : initialOffset + 876*(currentIndex-1)
     carousel.style.transform = `translateX(-${offset}px)`
     items[currentIndex].style.transform = 'scale(1)'
+    items[currentIndex+1].addEventListener('click', next)
+    if (currentIndex-1 >=0) items[currentIndex-1].addEventListener('click', previous)
+  }
+
+  const showButtons = () => {
+    buttonLeft.style.visibility = 'visible'
+    buttonRight.style.visibility = 'visible'
+  }
+
+  const hideButtons = () => {
+    buttonLeft.style.visibility = 'hidden'
+    buttonRight.style.visibility = 'hidden'
   }
 
   buttonLeft.addEventListener('click', previous)
   buttonRight.addEventListener('click', next)
+
+  const carouselContainer = document.querySelector('#carousel')
+
+  carouselContainer.addEventListener('mouseenter', showButtons)
+  carouselContainer.addEventListener('mouseleave', hideButtons)
 
 })()
